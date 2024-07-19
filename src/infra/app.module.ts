f2c8a3ddf861454228e96eb0d61f6b4974d42aad
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-18 22:44:05 -0300

    Creating EnvModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
+import { EnvService } from './env/env.service'
 import { HttpModule } from './http/http.module'
 
 @Module({
@@ -12,6 +15,8 @@ import { HttpModule } from './http/http.module'
     }),
     AuthModule,
     HttpModule,
+    EnvModule,
   ],
+  providers: [EnvService],
 })
 export class AppModule {}
