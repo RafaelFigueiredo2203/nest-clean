91c23a3c8ca1539e238d3004a6a5ad18c38ee415
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-08-03 11:45:49 -0300

    Creating Redis Service

---
+++ src/infra/cache/redis/redis.service.ts
@@ -0,0 +1,16 @@
+import { EnvService } from '@/infra/env/env.service'
+import { Redis } from 'ioredis'
+
+export class RedisService extends Redis {
+  constructor(envService: EnvService) {
+    super({
+      host: envService.get('REDIS_HOST'),
+      port: envService.get('REDIS_PORT'),
+      db: envService.get('REDIS_DB'),
+    })
+  }
+
+  onModuleDestroy() {
+    return this.disconnect()
+  }
+}
